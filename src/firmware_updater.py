f5784ad117b5a6ba07b8e093b1e67f113554683a
Trent Waddington <trent.waddington@gmail.com>
2014-11-24 07:37:35 +1000

    don't download older firmware.

--- src/firmware_updater.py
+++ src/firmware_updater.py
@@ -2,6 +2,7 @@ import requests
 import json
 import hashlib
 import os, os.path
+from setup import BASEVERSION
 
 firmware_base_url = 'http://update.shiel.io/firmware/'
 firmware_index = 'firmware.txt'
@@ -70,10 +71,31 @@ def download_file(filename, calculate_sha=True):
 
     return None
 
+def is_older_firmware(firmware):
+    if firmware.find('+') != -1:
+        parts = parts[:firmware.find('+')]
+    parts = firmware.split('.')
+    version = BASEVERSION.split('+')
+    version = version[0].split('.')
+    if int(parts[0]) < int(version[0]):
+        return True
+    if int(parts[0]) == int(version[0]):
+        if int(parts[1]) < int(version[1]):
+            return True
+        if int(parts[1]) == int(version[1]):
+            if len(parts) == 3:
+                return True
+            if int(parts[2]) < int(version[2]):
+                return True
+    return False
+
 def update_local_firmware():
     firmware_list = get_unavailable_firmware()
 
     for firmware in firmware_list:
+        if is_older_firmware(firmware['version']):
+            print("Not downloading older {}.".format(firmware['filename']))
+            continue
         print("Downloading {}.".format(firmware['filename']))
         result = download_file(firmware['filename'])
 
