51d589c2c87ec3c3d72397c9271a2f0d28b91ad7
Trent Waddington <trent.waddington@gmail.com>
2015-02-19 17:13:37 -0500

    bump version.

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ import time
 APP = 'gui.py'
 NAME = 'Argentum Control'
 
-BASEVERSION = '0.16.7'
+BASEVERSION = '0.16.8'
 BUILDTIME = time.strftime('%Y%m%d')
 
 VERSION = BASEVERSION + '+' + BUILDTIME
