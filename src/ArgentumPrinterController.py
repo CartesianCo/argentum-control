323514eab3c1a027d06e92a166f905541139a4ee
Trent Waddington <trent.waddington@gmail.com>
2015-02-24 11:13:07 -0500

    Apply GPL.

--- src/ArgentumPrinterController.py
+++ src/ArgentumPrinterController.py
@@ -1,3 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Argentum Control GUI
+
+    Copyright (C) 2013 Isabella Stevens
+    Copyright (C) 2014 Michael Shiel
+    Copyright (C) 2015 Trent Waddington
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from PrinterController import PrinterController
 import serial
 import hashlib
--- src/PrintView.py
+++ src/PrintView.py
@@ -2,9 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """
-Argentum Control PrintView
+    Argentum Control GUI
 
-author: Trent Waddington
+    Copyright (C) 2013 Isabella Stevens
+    Copyright (C) 2014 Michael Shiel
+    Copyright (C) 2015 Trent Waddington
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
